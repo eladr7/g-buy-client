9e61497d2caa9389d66fd00a7d9e8cc33829de3e
elad <elad@scrtlabs.com>
2022-04-18 15:38:35 +0300

    Implemented the secretjs client context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { SecretjsContextProvider } from "./components/context/CalculationsContext";
 
 interface AppProps {}
 const App: React.FC<AppProps> = ({}) => {
   return (
     <div className="App">
-      <header className="App-header">BLAST!</header>
+      <header className="App-header">
+        <SecretjsContextProvider>BLAST!</SecretjsContextProvider>
+      </header>
     </div>
   );
 };
---
+++ src/components/context/CalculationsContext.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from "react";
+import { SecretNetworkClient } from "secretjs";
+
+// This endpoint is a reverse proxy for a main-net scrt node
+const NODE_URL = "https://elad.uksouth.cloudapp.azure.com";
+const CHAIN_ID = "secret-4";
+// const CONTRACT_ADDRESS = "";
+// const CODE_ID = ;
+
+declare global {
+  interface Window {
+    keplr: any;
+    getOfflineSignerOnlyAmino: any;
+    getEnigmaUtils: any;
+  }
+}
+
+interface IContextProps {
+  secretjs: SecretNetworkClient | undefined;
+}
+
+export const SecretjsContext = createContext({} as IContextProps);
+
+interface SecretjsContextProps {}
+export const SecretjsContextProvider: React.FC<SecretjsContextProps> = (
+  props
+) => {
+  const [secretjs, setSecretjs] = useState<SecretNetworkClient>();
+
+  const setupKeplr = async () => {
+    const sleep = (ms: number) =>
+      new Promise((accept) => setTimeout(accept, ms));
+
+    // Wait for Keplr to be injected to the page
+    while (
+      !window.keplr ||
+      !window.getOfflineSignerOnlyAmino ||
+      !window.getEnigmaUtils
+    ) {
+      await sleep(10);
+    }
+
+    // Enable Keplr.
+    // This pops-up a window for the user to allow keplr access to the webpage.
+    await window.keplr.enable(CHAIN_ID);
+
+    const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(CHAIN_ID);
+    const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
+
+    let secretjs = await SecretNetworkClient.create({
+      grpcWebUrl: NODE_URL,
+      chainId: CHAIN_ID,
+      wallet: keplrOfflineSigner,
+      walletAddress: myAddress,
+      encryptionUtils: window.getEnigmaUtils(CHAIN_ID),
+    });
+
+    setSecretjs(secretjs);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await setupKeplr();
+    })();
+  }, []);
+
+  return (
+    <SecretjsContext.Provider value={{ secretjs }}>
+      {props.children}
+    </SecretjsContext.Provider>
+  );
+};
